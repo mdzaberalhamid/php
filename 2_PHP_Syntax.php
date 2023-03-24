a5899707c0a167ca60666572275f391add37230d
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-25 00:33:24 +0600

    Create 2_PHP_Syntax.php

---
+++ 2_PHP_Syntax.php
@@ -0,0 +1,19 @@
+<?php
+
+// Basic PHP syntax
+echo "Hello World!\n";
+
+// PHP case sensitivity
+// Case sensitivity does not occur for keywords, classes, functions name
+echo "A";
+ECHO "B";
+eChO "C\n";
+
+// PHP variable names are case sensitive
+$color = "Green";
+
+echo $color;  // Green
+echo $COLOR;  // No output
+echo $coLoR;  // No output
+
+?>
