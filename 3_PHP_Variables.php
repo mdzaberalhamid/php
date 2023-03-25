d144b9603ff619f3a14eb7f6401a6a84e03d1f06
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-25 23:04:32 +0600

    Create 3_PHP_Variables.php

---
+++ 3_PHP_Variables.php
@@ -0,0 +1,29 @@
+<?php
+// PHP Variables
+
+// Creating PHP variables
+$msg = "Hi";
+$num = 1;
+$numD = 3.43;
+
+echo $msg . "   ";
+echo $num . "   ";
+echo $numD;
+echo "\n";
+
+// Variable names are case sensitive
+$age = 29;
+$AGE = 19;
+
+echo $age . "   ";
+echo $AGE;
+echo "\n";
+
+// Output a variable
+$txt = "PHP";
+echo "I love $txt \n";
+echo "I like to code in " . $txt . "!\n";
+$x = 2;
+$y = 3;
+echo $x + $y;
+?>
