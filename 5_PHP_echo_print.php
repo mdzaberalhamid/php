fb9f6b6de17c019f9754e2d3f395b5bf298be837
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-26 16:54:14 +0600

    Create 5_PHP_echo_print.php

---
+++ 5_PHP_echo_print.php
@@ -0,0 +1,33 @@
+<?php
+// PHP echo and print
+
+// PHP echo statement
+// Output text
+echo "PHP is fun.\n";
+echo "Hello World!\n";
+echo "I am learning PHP.\n";
+echo "PHP ", "is ", "easy ", "to ", "learn!", "\n";
+
+// Output variables
+$txt1 = "Hi";
+$txt2 = "I am Zaber.";
+$a = 1;
+$b = 4;
+
+echo $txt1 . "," . $txt2 . "\n";
+echo $a + $b;
+echo "\n";
+
+// PHP print statement
+// Output text
+print "This is a text.\n";
+print "This is another line of text!\n";
+
+// Output variables
+$txt3 = "Text in a variable!";
+$num = 25;
+print "$txt3" . "\n";
+print $num . "\n";
+print $num + 2;
+
+?>
