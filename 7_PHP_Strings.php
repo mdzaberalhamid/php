20d72cf36c679f99c32444f13bc2d61f0bc73cb9
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-28 14:55:32 +0600

    Create 7_PHP_Strings.php

---
+++ 7_PHP_Strings.php
@@ -0,0 +1,24 @@
+<?php
+// PHP strings
+
+// PHP string functions
+// strlen() - returns length of a string
+echo strlen("Hello World!");  // 12
+echo "\n";
+
+// str_word_count() - count words in a string
+echo str_word_count("Hello World!");  // 2
+echo "\n";
+
+// strrev() - reverse a string
+echo strrev("Hi, friends!");  // !sdneirf ,iH
+echo "\n";
+
+// strpos() - search a word in a string
+echo strpos("Hello World.", "World");  // 6
+echo "\n";
+
+// str_replace - replace text in a string
+echo str_replace("World", "Friend", "Hello World!");  // Hello Friend!
+
+?>
