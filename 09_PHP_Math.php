766d4f4ff4907910e677f70126489004ea05be3e
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-07-01 21:16:57 +0600

    Create 09_PHP_Math.php

---
+++ 09_PHP_Math.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "PHP Math\n";
+echo "Math Functions:\n";
+echo (pi());
+
+echo "\n";
+echo (min(1, 35, 200, -50, -500, 99));
+
+echo "\n";
+echo (max(1, 35, 200, -50, -300, 99));
+
+echo "\n";
+echo (abs(-9.76));
+
+echo "\n";
+echo (sqrt(64));
+
+echo "\n";
+echo (round(0.51));
+echo "\n";
+echo (round(0.49));
+
+echo "\n";
+echo (rand());
+echo "\n";
+echo (rand(1,10));
+
+?>
