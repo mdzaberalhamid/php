8686dedc864837e6fbdce32fda76fda496469527
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-07-11 21:04:29 +0600

    Create 13_PHP_Switch_Statement.php

---
+++ 13_PHP_Switch_Statement.php
@@ -0,0 +1,35 @@
+<?php
+
+echo "PHP switch Statement\n";
+
+echo "\nExample:\n";
+
+$day = 3;
+
+switch ($day) {
+  
+  case 1:
+  echo "Monday\n";
+  break;
+
+  case 2:
+  echo "Tuesday\n";
+  break;
+
+  case 3:
+  echo "Wednesday\n";
+  break;
+
+  case 4:
+  echo "Thursday\n";
+  break;
+
+  case 5:
+  echo "Friday\n";
+  break;
+
+  default:
+  echo "Rest Day!\n";
+}
+
+?>
