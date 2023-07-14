084b61d0dc60ba35481b266cbaefe2683034ba72
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-07-14 22:28:38 +0600

    Create 16_PHP_Arrays(1).php

---
+++ 16_PHP_Arrays(1).php
@@ -0,0 +1,14 @@
+<?php
+
+echo "PHP Arrays\n";
+
+echo "Example:\n";
+$cars = array("marcedes","nissan","volvo");
+echo "I like " . $cars[0] . ", " . $cars[1] . ", and " . $cars[2] . " brand cars!\n";
+
+echo "\nCount length of an array:\n";
+echo "Example:\n";
+$colors = array("red","green","yellow");
+echo count($colors);
+
+?>
